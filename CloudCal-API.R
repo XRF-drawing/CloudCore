2fc006d466eff04f0c3d40544485de98c2fef0a5
Lee Drake <b.lee.drake@gmail.com>
2019-11-09 11:03:07 -0700

    Maintenance Updates
    just keeping this thing alive

---
+++ CloudCal-API.R
@@ -0,0 +1,9 @@
+###API for CloudCal
+list.of.packages <- c("devtools")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos="http://cran.rstudio.com/", dep = TRUE)
+
+library(devtools)
+
+#source_url("https://raw.githubusercontent.com/leedrake5/CloudCal/master/global.R")
+source("~/GitHub/CloudCal/global.R", chdir=TRUE)
